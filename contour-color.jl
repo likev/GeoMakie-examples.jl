3b9e8130287528732397d9c369a724bcd25593f7
likev <3146974+likev@users.noreply.github.com>
2024-05-09 10:59:50 +0000

    a repeating pattern of 5 colors for 20 points using Makie

---
+++ contour-color.jl
@@ -0,0 +1,8 @@
+using CairoMakie
+
+# a practical solution to ensure that each point is assigned one of the 5 discrete colors in a repeating pattern.
+f, ax, sc = scatter(range(1, 7, 20), fill(0, 20),color=(0:19) .% 5 .+ 1, colormap=:Spectral_5, markersize = 50)
+
+text!(ax, 4, 0, text = ":Spectral_5", align = (:center, :bottom), offset = (0, 30))
+
+save("scatter20.png", current_figure())
