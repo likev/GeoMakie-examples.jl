e84b74d64a01f6d2482482ec9b8a965b489d3926
likev <3146974+likev@users.noreply.github.com>
2024-05-10 01:59:51 +0000

    using repeat to create a sequence of 20 elements that loops from 1 to 5

--- contour-color.jl
+++ contour-color.jl
@@ -1,7 +1,7 @@
 using CairoMakie
 
 # a practical solution to ensure that each point is assigned one of the 5 discrete colors in a repeating pattern.
-f, ax, sc = scatter(range(1, 7, 20), fill(0, 20),color=(0:19) .% 5 .+ 1, colormap=:Spectral_5, markersize = 50)
+f, ax, sc = scatter(range(1, 7, 20), fill(0, 20), color=repeat(1:5, 4), colormap=:Spectral_5, markersize = 50)
 
 text!(ax, 4, 0, text = ":Spectral_5", align = (:center, :bottom), offset = (0, 30))
 
